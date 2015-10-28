574d90e996594df8aa23f3dd2a885cbd4e5afccf
Oleg <shinkarenko_oleg@mail.ua>
2015-10-29 00:07:36 +0200

    Created camera movement.

---
+++ Clash/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	public int speed = 100;
+
+	public float leftRestriction = 354;
+	public float rightRestriction = 48;
+	public float upRestriction = 353;
+	public float downRestriction = 45;
+
+	// Update is called once per frame
+	void Update () {
+		if ((transform.position.z <= leftRestriction) && (int)Input.mousePosition.x < 2)
+			transform.position -= transform.right * Time.deltaTime * speed;
+		if ((transform.position.z >= rightRestriction) && (int)Input.mousePosition.x > Screen.width - 2)
+			transform.position += transform.right * Time.deltaTime * speed;
+
+
+
+	/*	if ((transform.position.z <= leftRestriction) && (int)Input.mousePosition.x < 2)
+			transform.position += transform.forward * Time.deltaTime * speed;
+		if ((transform.position.z >= rightRestriction) && (int)Input.mousePosition.x > Screen.width - 2)
+			transform.position -= transform.forward * Time.deltaTime * speed;*/
+
+		/*if ((transform.position.x <= upRestriction) && Input.mousePosition.y > Screen.height - 2)
+			transform.position += transform.right * Time.deltaTime * speed;
+		if ((transform.position.x >= downRestriction) && Input.mousePosition.y < 2)
+			transform.position -= transform.right * Time.deltaTime * speed;*/
+	}
+}
---
+++ Clash/Assets/Scripts/TreeCreator.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TreeCreator : MonoBehaviour {
+
+	public Transform prefab;
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 17; i++) {
+			Instantiate(prefab, new Vector3(i * 8F + 7F, 0, - 5F), Quaternion.identity);
+		}
+		for (int j = 0; j < 10; j++) {
+			Instantiate(prefab, new Vector3(7F, 0, j * 8F + 3F), Quaternion.identity);
+		}
+		for (int k = 0; k < 10; k++) {
+			Instantiate(prefab, new Vector3(135F, 0, k * 8F + 3F), Quaternion.identity);
+		}
+		for (int z = 0; z < 15; z++) {
+			Instantiate(prefab, new Vector3(z * 8F + 15F, 0, 75F), Quaternion.identity);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
